5f80a8fc2b6b8d0cb8e505b7872631116294661a
Alimardon <alimardon007@gmail.com>
2023-02-01 17:54:21 +0300

    update function on read Items and add Item (with reload)

--- lib/main.dart
+++ lib/main.dart
@@ -35,36 +35,27 @@ class _MyHomePageState extends State<MyHomePage> {
   Person personOne = Person(name: "Anton", surname: "Krasnih", age: 31);
 
   Future<void> readJsonData() async {
-    final jsonData = await rootBundle.loadString('assets/example.json');
-    final list = jsonDecode(jsonData);
+    final File file = File('assets/example.json');
+    if (!await file.exists()) await file.create();
 
+    final String jsonData = await file.readAsString();
+    final list = jsonDecode(jsonData);
     list
         .map((dynamic e) => Person.fromJson(e as Map<String, dynamic>))
         .toList();
 
-    setState(() {
-      _items = list;
-    });
+    setState(() => _items = list);
   }
 
-  Future<void> addJsonData(newPerson) async {
-    final person = newPerson.toJson();
-
-    setState(() {
-      _items.add(person);
-    });
+  Future<void> addJsonData(Person newPerson) async {
     final File file = File('assets/example.json');
+    if (!await file.exists()) await file.create();
 
-    //! first example
-    // final jsonDataUpdated = jsonEncode(_items); // !правильный формат, почему-то не хочет дальше перезаписывать
-    // await file.writeAsString(jsonDataUpdated);
+    final person = newPerson.toJson();
+    setState(() => _items.add(person));
 
-    //! second example
-
-    String content = await file.readAsString();
-    List myList = jsonDecode(content);
-    myList.add(person);
-    await file.writeAsString(jsonEncode(myList));
+    final jsonDataUpdated = jsonEncode(_items);
+    await file.writeAsString(jsonDataUpdated);
   }
 
   @override
@@ -78,18 +69,21 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'People list:',
             ),
             Text(
               '$_items',
             ),
             SizedBox(height: 40),
             ElevatedButton(
-                onPressed: () => readJsonData(),
-                child: Icon(Icons.data_saver_off_rounded)),
+              onPressed: () => readJsonData(),
+              child: Text("Load data"),
+            ),
             SizedBox(height: 10),
             ElevatedButton(
-                onPressed: () => addJsonData(personOne), child: Icon(Icons.add))
+              onPressed: () => addJsonData(personOne),
+              child: Text("Add person"),
+            )
           ],
         ),
       ),
