27cfac1cb7c72218e71363317b20103e96f71fdc
kiriruru <kirill@achiever.software>
2023-02-20 21:02:27 +0100

    Update chosen_cubit.dart

--- lib/cubit/chosen_cubit.dart
+++ lib/cubit/chosen_cubit.dart
@@ -1,7 +1,30 @@
 import 'package:bloc/bloc.dart';
 
 class ChosenUserCubit extends Cubit<String> {
-  ChosenUserCubit(String isUserChosen) : super(isUserChosen);
+  final DataSource ds;
+  ChosenUserCubit(this.ds) : super(ChosenUserCubitStateInit());
 
-  void choseUser(String id) => emit(id);
+  Future<void> choseUser(String id) async {
+    emit(ChosenUserCubitStateLoading());
+    // получаем данные от покетбейза + парсинг json
+    // final t = await ds.getData(); 
+    emit(ChosenUserCubitStateReady(t));
+  }
 }
+
+class ChosenUserCubitState {}
+class ChosenUserCubitStateInit extends ChosenUserCubitState {}
+class ChosenUserCubitStateLoading extends ChosenUserCubitState {}
+class ChosenUserCubitStateReady extends ChosenUserCubitState {
+  final Map<String,Sring> data;
+  ChosenUserCubitStateReady(this.data);
+}
+class ChosenUserCubitStateError extends ChosenUserCubitState {}
+
+// в BlocBuilder:
+if(state is ChosenUserCubitStateInit){
+  // ...
+} else if(state is ChosenUserCubitStateLoading){
+  // show CircleProgressIndicator
+} else if(state is ChosenUserCubitStateready){
+  ...
