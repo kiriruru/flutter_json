68acc3bba22e11927d5d4e8f3334c19f5326ee50
kiriruru <kirill@achiever.software>
2023-03-07 14:58:08 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'app/PbAuth.dart';
 import '../runner.dart';
 
+final pocketBase = PocketBase("http://127.0.0.1:8090");
+  
 void main() async {
-  await PbAuth.auth();
-  runner();
+  await pocketBase.admins.authWithPassword('alimardon007@gmail.com', '5544332211');
+  runner(pocketBase);
 }
